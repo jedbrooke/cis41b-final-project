7d3e9d70e30cbfe4acca0a780dcc490c4a75f157
Jasper Edbrooke <jasperredrock@gmail.com>
2019-06-10 16:18:45 -0700

    lists
    added support for scrolling lists and fixed lists

--- gui_engine.py
+++ gui_engine.py
@@ -20,11 +20,22 @@ GRID_ARGS = {
 	"row":int,
 	"column":int,
 }
-def get_grid(tag):
-	return dict([(arg,get_attribute(tag,arg,kind))\
-	 for arg,kind in GRID_ARGS.items()\
-	  if get_attribute(tag,arg,kind) is not None])
+LISTBOX_ARGS = {
+	"height":int,
+	"width":int,
+	"selectmode":str,
+}
+def get_grid_args(tag):
+	return dict(\
+		[(arg,get_attribute(tag,arg,kind))\
+	 	for arg,kind in GRID_ARGS.items()\
+	  	if get_attribute(tag,arg,kind) is not None])
 
+def get_listbox_args(tag):
+	return dict(\
+		[(arg,get_attribute(tag,arg,kind))\
+	 	for arg,kind in LISTBOX_ARGS.items()\
+	  	if get_attribute(tag,arg,kind) is not None])
 
 class window(tk.Tk):
 	"""docstring for window"""
@@ -38,6 +49,8 @@ class window(tk.Tk):
 			"button":self.create_button,
 			"label":self.create_label,
 			"frame":self.create_frame,
+			"scrollbox":self.create_scrollbox,
+			"listbox":self.create_listbox,
 		}
 		super().__init__()
 		self.grab_set()
@@ -64,6 +77,9 @@ class window(tk.Tk):
 		for tag in soup:
 			if tag.name is not None:
 				self.BODY_ACTIONS[tag.name](tag,parent)
+	def buildList(self,elements,listbox):
+		listbox.insert(tk.END,*[item.text for item in elements.find_all("li")])
+
 
 	def set_title(self,title):
 		self.title(title.text)
@@ -72,15 +88,37 @@ class window(tk.Tk):
 		self.geometry(geometry.text)
 
 	def create_label(self,label,parent):
-		tk.Label(parent,text=label.text.strip()).grid(get_grid(label))
+		tk.Label(parent,text=label.text.strip()).grid(get_grid_args(label))
 
 	def create_button(self,button,parent):
-		tk.Button(parent,text=button.text.strip(),command=lambda:print("hello")).grid(get_grid(button))
+		tk.Button(parent,text=button.text.strip(),command=lambda:print("hello")).grid(get_grid_args(button))
 
 	def create_frame(self,frame,parent):
 		tk_frame = tk.Frame(parent)
 		self.buildBody(frame,tk_frame)
-		tk_frame.grid(get_grid(frame))
+		tk_frame.grid(get_grid_args(frame))
+
+	def create_listbox(self,listbox,parent): 
+		if get_attribute(listbox,"scroll",bool):
+			self.create_scrollbox(listbox,parent)
+		else :
+			tk_listbox = tk.Listbox(parent,**get_listbox_args(listbox))
+			self.buildList(listbox,tk_listbox)
+			tk_listbox.grid(get_grid_args(listbox))
+			
+
+
+	def create_scrollbox(self,scrollbox,parent):
+		frame = tk.Frame(parent)
+		scrollbar = tk.Scrollbar(frame,orient=tk.VERTICAL)
+		scrollbar.grid(row=0,column=1,sticky=tk.N+tk.S)
+		listbox = tk.Listbox(frame,**get_listbox_args(scrollbox),yscrollcommand=scrollbar.set)
+		listbox.grid(row=0,column=0,sticky=tk.N+tk.S+tk.E+tk.W)
+		self.buildList(scrollbox,listbox)
+		scrollbar['command'] = listbox.yview
+		frame.grid(get_grid_args(scrollbox))
+
+
 
 
 
