248b85f919cee1c1b42945eca8d10506ea74ac87
Tung X. Dao <tungxdao.gmail.com>
2019-06-12 14:17:12 -0700

    Set up db creation

--- data.py
+++ data.py
@@ -3,12 +3,17 @@ Tung X. Dao and Jasper Edbrooke
 data.py handles the file storage and retrieval for the GUI
 """
 import sqlite3
+import sys
 
-class sqldb():
+class SqlDb():
     # Set up connection
-
+    DBNAME = 'images.db'
     def __init__(self):
-        pass
+        """ 
+        Set up db if none is found
+        """
+        # Check for existance of db
+        self.create_db()
 
     def add_to_db(self, image, metadata):
         pass
@@ -23,4 +28,39 @@ class sqldb():
         pass
 
     def create_db(self):
-        pass
+        self.conn = sqlite3.connect(self.DBNAME)
+        self.cur = self.conn.cursor()
+
+        tables = ['Images', 'Image_Categories', 'Categories']
+
+        for table in tables:
+            # self.cur.execute('DROP TABLE IF EXISTS ?', (table,)) # Why does this not work?
+            self.cur.execute('DROP TABLE IF EXISTS {};'.format(table))
+
+        # Create tables
+        try:
+            self.cur.execute('''CREATE TABLE Images (
+                        id INTEGER NOT NULL PRIMARY KEY,
+                        file BLOB,
+                        url TEXT,
+                        nsfw INTEGER                        
+                        );''')
+
+            self.cur.execute('''CREATE TABLE Categories (
+                        id INTEGER NOT NULL PRIMARY KEY,
+                        category TEXT                        
+                        );''')
+
+            self.cur.execute('''CREATE TABLE Image_Categories (
+                        img_id INTEGER NOT NULL,
+                        category_id INTEGER NOT NULL,
+                        FOREIGN KEY (img_id) REFERENCES Images(id),
+                        FOREIGN KEY (category_id) REFERENCES Categories(id)
+                        );''')
+        except sqlite3.OperationalError as e:
+            print(str(e))
+            raise SystemExit
+        #
+
+if __name__ == "__main__":
+    db = SqlDb()
