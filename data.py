4c4d82e99f86408b7da045a1bdca0514d022373a
Tung X. Dao <tungxdao.gmail.com>
2019-06-12 13:30:56 -0700

    Initial skeleton

---
+++ data.py
@@ -0,0 +1,23 @@
+""" 
+Tung X. Dao and Jasper Edbrooke
+data.py handles the file storage and retrieval for the GUI
+"""
+import sqlite3
+
+class sqldb():
+    # Set up connection
+
+    def __init__(self):
+        pass
+
+    def add_to_db(self, image):
+        pass
+
+    def get_image(self, img_id):
+        pass
+
+    def get_count_of_tag(self, tag):
+        pass
+
+    def delete_image(self, img_id):
+        pass
