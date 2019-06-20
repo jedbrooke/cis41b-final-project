bd2d6ea1c0c9c5354d3073877227981326f7064e
txdao <tungxdao@gmail.com>
2019-06-19 18:18:17 -0700

    Big update

--- client_network_test.py
+++ client_network_test.py
@@ -5,7 +5,7 @@ import pickle
 HOST = '127.0.0.1'
 PORT = 5551
 
-def get_choice():
+def get_choice(options):
     """ 
     Make sure user enters sane option 
     """
@@ -16,22 +16,27 @@ def get_choice():
     print('q: quit')
     choice = ''
 
-    while choice not in tuple('psq'):
+    while choice not in tuple('1234q'):
         choice = input('Enter your choice: ')
+    if choice == 'q':
+        pass
+    else:
+        choice = [i for i in options.keys()][int(choice) - 1]
     
     return choice
 
-def send_data():
-    pass
+def send_data(data_dict):
 
-def clear_db():
-    pass
+    s.send(pickle.dumps(data_dict))
 
-def check_db_for_training():
-    pass
+def clear_db(data_dict):
+    s.send(pickle.dumps(data_dict))
 
-def train_network():
-    pass
+def check_db_for_training(data_dict):
+    s.send(pickle.dumps(data_dict))
+
+def train_network(data_dict):
+    s.send(pickle.dumps(data_dict))
 
 with socket.socket() as s: # create a socket
     s.connect((HOST, PORT)) # connect to a server at a particular host and port
@@ -40,13 +45,14 @@ with socket.socket() as s: # create a socket
     options = {'send_data': send_data, 
                 'clear_db': clear_db,
                 'check_if_trainable': check_db_for_training,
-                'train': train_network}
-    choice = get_choice()
+                'train': train_network,
+                }
+    choice = get_choice(options)
 
-    while choice in options.keys():
-        data_dict = {'type': choice}
+    while choice in options.keys() or choice == 'q':
+        data_dict = {'command': choice}
         if choice == 'q':
             s.send(pickle.dumps(data_dict))
             break
         options[choice](data_dict)
-        choice = get_choice()
+        choice = get_choice(options)
