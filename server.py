119489eef881ab618ebafeb4c1b7b2bec59c4dfc
txdao <tungxdao@gmail.com>
2019-06-19 17:23:00 -0700

    Commit of a lot of changes.

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ This server handles database checking and downloading of the large full sized fi
 """
 
 import socket
-import serverdata
+import serverdb
 import pickle
 
 HOST = 'localhost'
@@ -12,6 +12,9 @@ PORT = 5551
 
 class Server():
     def __init__(self, timeout = 60):
+        # Initialize db
+        self.db = serverdb.SqlDb()
+
         with socket.socket() as s:
             try:
                 # Listen for the clients
@@ -28,10 +31,11 @@ class Server():
                 while True:
                     from_client = pickle.loads(conn.recv(1024))
 
-                    if from_client['type'] == 'q':
+                    if from_client['command'] == 'q':
+                        print('goodbye')
                         break
                     else:                        
-                        options[from_client['type']](from_client, conn)   
+                        options[from_client['command']](from_client)  
 
             except socket.timeout:
                 print('time out')
@@ -41,26 +45,49 @@ class Server():
         """  
         Gets the information of which files to download from client
         """
-        pass
+        print('Getting data from client')
+        urls = args['data'][0]
+        tags = args['data'][1]
 
-    def clear_db(self, db):
+        # download and add urls and tags to DB
+        print('downloading to db')
+        for url, tag in zip(urls, tags):
+            self.db.add_to_db(url, tag)
+
+        
+
+    def clear_db(self):
         """  
         Resets the db
         """
-        db.create_db()
+        self.db.create_db()
 
     def check_db_for_training(self):
         """  
         Checks the db for sufficient data to start training
         Suppose sufficient data is >= 2 categories and >= 10 images for testing purposes
         """
-        pass
+        n_categories = self.db.cur.execute('''SELECT COUNT(c.id) FROM categories c;''').fetchone()
+        n_images = self.db.cur.execute('SELECT COUNT(i.id) FROM Images i;').fetchone()
+        
+        ready = False
+        if n_categories < 2 and n_images < 10:
+            print('DB needs more categories and images')
+        elif n_images < 10: # Check this n_images condition
+            print('DB needs more images')
+        elif n_categories < 2:
+            print('DB needs more categories')
+        else:
+            print('DB is good to go')
+            ready = True
+
+        return ready
 
     def train_network(self):
         """  
         Check if sufficient data in the local db and then print message about training
         """
-        pass
+        print('Training in progress. Please come back in a few hours.')
 
 if __name__ == "__main__":
     s = Server()
